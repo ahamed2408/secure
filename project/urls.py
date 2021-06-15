030dc3752f41d29cf27a5109c983677e4fdc4d71
unknown <ahamed.irshad24@gmail.com>
2021-06-15 18:59:26 +0530

    First commit

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.urls import path,include
-
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registers/', user_views.registers, name='registers'),
     path('', include('blogs.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
