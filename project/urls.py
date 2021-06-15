62d1dc5832ebc97f0b8053574f2b9083d959a115
unknown <ahamed.irshad24@gmail.com>
2021-06-15 18:48:04 +0530

    First commit

--- project/urls.py
+++ project/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogs.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
