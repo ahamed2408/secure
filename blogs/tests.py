296c0759dfd3634814a7d16e4e0196a738441991
unknown <ahamed.irshad24@gmail.com>
2021-06-21 13:39:40 +0530

    Test All 4 tests

--- blogs/tests.py
+++ blogs/tests.py
@@ -6,10 +6,6 @@ from .models import shipd, orderss
 # django 2.x
 from django.urls import reverse
 
-def test_create(self):
-    response = self.client.get(reverse('create', args=[self.userName]))
-    self.assertEqual(response.status_code, 200)
-
 import datetime
 
 # Create your tests here.
