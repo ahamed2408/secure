0a01470dd5f4ff8f806e15d6977ac91da59eced1
unknown <ahamed.irshad24@gmail.com>
2021-06-21 18:29:45 +0530

    Test New 4

--- users/tests.py
+++ users/tests.py
@@ -4,8 +4,8 @@ from django.test import TestCase
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
-            'username': 'sealanes',
-            'password': 'Seauser@123'}
+            'username': 'sealanesss',
+            'password': 'Seauser@123ss'}
         User.objects.create_user(**self.credentials)
     def test_login(self):
         # send login data
