98e7009e797085f62100a82dd5e13fd615cfd499
unknown <ahamed.irshad24@gmail.com>
2021-06-21 13:43:09 +0530

    New 3

--- users/tests.py
+++ users/tests.py
@@ -4,11 +4,11 @@ from django.test import TestCase
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
+            'username': 'ahamed',
+            'password': 'Momdad786!'}
         User.objects.create_user(**self.credentials)
     def test_login(self):
         # send login data
         response = self.client.post('/login/', self.credentials, follow=True)
         # should be logged in now
-        self.assertTrue(response.context['user'].is_active)
+        self.assertTrue(response.context['user'].is_authenticated)
