e693579d98eebe193f5bddadcf0d625ecc840ab5
unknown <ahamed.irshad24@gmail.com>
2021-06-21 16:05:25 +0530

    All test - Testing

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import os
-
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,35 +88,60 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deo2h6rl5cjn11',
-        'HOST' : 'ec2-52-5-1-20.compute-1.amazonaws.com',
-        'PORT' : 5432,
-        'USER' : 'wtikehgsaawspm',
-        'PASSWORD' : '853d155860e702fdab2725cddd1759b245ed2a0336c0bce80f332a3c25212ee1',
-        'TEST':{
-            'NAME': 'd8irecume0lp11',
-        }
-    }
-}
-
-if os.environ.get('GITHUB_WORKFLOW'):
+if 'test' in sys.argv:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deo2h6rl5cjn11',
-        'HOST' : 'ec2-52-5-1-20.compute-1.amazonaws.com',
+        'NAME': 'd8irecume0lp11',
+        'HOST' : 'ec2-54-145-224-156.compute-1.amazonaws.com',
         'PORT' : 5432,
-        'USER' : 'wtikehgsaawspm',
-        'PASSWORD' : '853d155860e702fdab2725cddd1759b245ed2a0336c0bce80f332a3c25212ee1',
+        'USER' : 'nkbvzhiqrkzsay',
+        'PASSWORD' : 'c1a95bf38b8125412b35d552a67ada6fa16e351c3ebb4e75b72e4c2cdd7a5551',
         'TEST':{
             'NAME': 'd8irecume0lp11',
         }
     }
 }
+
+    if os.environ.get('GITHUB_WORKFLOW'):
+        DATABASES = {
+            'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd8irecume0lp11',
+            'HOST' : 'ec2-54-145-224-156.compute-1.amazonaws.com',
+            'PORT' : 5432,
+            'USER' : 'nkbvzhiqrkzsay',
+            'PASSWORD' : 'c1a95bf38b8125412b35d552a67ada6fa16e351c3ebb4e75b72e4c2cdd7a5551',
+            'TEST':{
+                'NAME': 'd8irecume0lp11',
+            }
+    }
+}
+
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'deo2h6rl5cjn11',
+            'HOST' : 'ec2-52-5-1-20.compute-1.amazonaws.com',
+            'PORT' : 5432,
+            'USER' : 'wtikehgsaawspm',
+            'PASSWORD' : '853d155860e702fdab2725cddd1759b245ed2a0336c0bce80f332a3c25212ee1',
+        }
+    }
+
+    if os.environ.get('GITHUB_WORKFLOW'):
+        DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'deo2h6rl5cjn11',
+            'HOST' : 'ec2-52-5-1-20.compute-1.amazonaws.com',
+            'PORT' : 5432,
+            'USER' : 'wtikehgsaawspm',
+            'PASSWORD' : '853d155860e702fdab2725cddd1759b245ed2a0336c0bce80f332a3c25212ee1',
+        }
+    }
     
 
 
