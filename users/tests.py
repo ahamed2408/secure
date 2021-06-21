a2fb7517e6814fd9e1149d11fdbef4fd37ce4fa4
unknown <ahamed.irshad24@gmail.com>
2021-06-21 16:45:10 +0530

    Test New 4

--- users/tests.py
+++ users/tests.py
@@ -1,1 +1,15 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'sealanes',
+            'password': 'Seauser@123'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
 
