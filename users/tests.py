fc7d2fedb98bd70bb03759ac5e17b6823668c469
unknown <ahamed.irshad24@gmail.com>
2021-06-21 19:17:33 +0530

    Test New

--- users/tests.py
+++ users/tests.py
@@ -6,7 +6,6 @@ class LogInTest(TestCase):
         self.credentials = {
             'username': 'sealanes',
             'password': 'Seauser@123'}
-        User.objects.create_user(**self.credentials)
     def test_login(self):
         # send login data
         response = self.client.post('/login/', self.credentials, follow=True)
