3dfa3672dc0ed29b6f977c7eb4e92d09b8b2baa0
unknown <ahamed.irshad24@gmail.com>
2021-06-26 15:13:40 +0530

    Final Test Admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import analytics, orderss, shipd
 # Register your models here.
+
+My_models=[analytics, orderss, shipd]
+
+admin.site.register(My_models)
